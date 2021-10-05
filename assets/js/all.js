27adbbbc129bf405b27302dfd2b7721df00a0ec0
Joy Cheng <joy.cheng97324@gmail.com>
2021-10-05 16:13:36 +0800

    Update 2021-10-05T08:13:35.556Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -2,48 +2,53 @@
 
 var url = "https://hexschool.github.io/js-filter-data/data.json";
 var productList = document.querySelector(".show-list");
+var btnGroup = document.querySelector(".btn-group");
+var btnCropsType = document.querySelectorAll(".btn-group a");
 var inputTxt = document.querySelector(".input-txt");
 var inputBtn = document.querySelector(".input-btn");
 var resultName = document.querySelector(".result-name");
 var selectList = document.querySelectorAll(".select-group");
 var tableSortGroup = document.querySelector(".table-title");
-var data; //原始資料
-
-var showData = []; //用來放filter 後的結果
-//撈取資料
+var data;
+var filterData = []; //== axios撈取資料 ==//
 
 function getData() {
   axios.get(url).then(function (response) {
     data = response.data.filter(function (item) {
       return item.作物名稱;
-    }); //需要賦予data 資料
-    //renderData(data);
-
+    });
     init(); //非同步
   });
-} //預設渲染畫面
+} //== 預設渲染畫面 ==//
 
 
 function init() {
   productList.innerHTML = "<tr class=\"table-waiting\"><td colspan=\"7\">\u8ACB\u8F38\u5165\u4E26\u641C\u5C0B\u60F3\u6BD4\u50F9\u7684\u4F5C\u7269\u540D\u7A31^\uFF3F^</td></tr>";
+  inputBtn.addEventListener('click', searchCrops, false);
+  inputTxt.addEventListener('keyup', searchCropsKey, false);
+  btnGroup.addEventListener('click', filterCropType, false);
+  tableSortGroup.addEventListener('click', sortTableBytitle, false);
+  selectListAll();
+} //== select pc mobile 兩個下拉選單監聽事件 ==//
+
+
+function selectListAll() {
+  selectList.forEach(function (item, index) {
+    selectList[index].addEventListener("change", changeSelect, false);
+  });
 }
 
-getData(); //更新選染畫面使用函式
+getData(); //== 更新選染畫面使用函式 ==//
 
-function renderData(data) {
+function update(showData) {
   var str = '';
-  data.forEach(function (item) {
+  showData.forEach(function (item) {
     var content = "\n            <tr>\n            <td>".concat(item.作物名稱, "</td>\n            <td>").concat(item.市場名稱, "</td>\n            <td>").concat(item.上價, "</td>\n            <td>").concat(item.中價, "</td>\n            <td>").concat(item.下價, "</td>\n            <td>").concat(item.平均價, "</td>\n            <td>").concat(item.交易量, "</td>\n            </tr>\n            ");
     str += content;
   });
   productList.innerHTML = str;
-  selectListAll();
-} //btn type 初步篩選資料
+} //== 按鈕分類篩選資料 ==//
 
-
-var btnGroup = document.querySelector(".btn-group");
-var btnCropsType = document.querySelectorAll(".btn-group a");
-btnGroup.addEventListener('click', filterCropType, false);
 
 function filterCropType(e) {
   e.preventDefault();
@@ -58,46 +63,43 @@ function filterCropType(e) {
     e.target.classList.add("active");
   });
   var type = e.target.dataset.type;
-  showData = data.filter(function (item) {
+  filterData = data.filter(function (item) {
     return type === item.種類代碼;
   });
   resultName.textContent = e.target.textContent;
   resetSelect();
-  renderData(showData);
-} //搜尋資料
+  update(filterData);
+} //== 搜尋資料 ==//
 
-
-inputBtn.addEventListener('click', searchCrops, false);
-inputTxt.addEventListener('keyup', searchCropsKey, false);
 
 function searchCrops(e) {
-  //確認點擊位置
-  if (inputTxt.value.trim() === "") {
-    inputBtn.setAttribute("class", "input-btn");
+  if (inputTxt.value.trim() !== "") {
+    inputBtn.classList.add("btn-active");
+  } else {
     return;
   }
 
   ;
-  showData = data.filter(function (item) {
+  filterData = data.filter(function (item) {
     return item.作物名稱.match(inputTxt.value.trim());
   });
   btnCropsType.forEach(function (item) {
-    item.classList.remove("active");
+    return item.classList.remove("active");
   });
   resultName.textContent = inputTxt.value.trim();
   inputTxt.value = "";
-  inputBtn.setAttribute("class", "input-btn");
+  inputBtn.classList.remove("btn-active");
   resetSelect();
 
-  if (showData.length === 0) {
+  if (filterData.length === 0) {
     var content = "<tr class=\"table-waiting\"><td colspan=\"7\">\u67E5\u8A62\u4E0D\u5230\u4EA4\u6613\u8CC7\u8A0AQQ</td></tr>";
     productList.innerHTML = content;
     return;
   }
 
   ;
-  renderData(showData);
-} //Enter 搜尋
+  update(filterData);
+} //== Enter Keyup 鍵盤搜尋 ==//
 
 
 function searchCropsKey(e) {
@@ -113,18 +115,11 @@ function searchCropsKey(e) {
   }
 
   ;
-} //select change 下拉選單 事件監聽
-//selectList.addEventListener("change", changeSelect, false);
+} //== select change 下拉選單 ==//
 
-
-function selectListAll() {
-  selectList.forEach(function (item, index) {
-    selectList[index].addEventListener("change", changeSelect, false);
-  });
-}
 
 function changeSelect(e) {
-  if (showData.length == 0) {
+  if (filterData.length == 0) {
     return;
   }
 
@@ -151,18 +146,16 @@ function changeSelect(e) {
       sortChange("交易量");
       break;
   }
-} //執行sort 函式
+} //==  下拉選單 排序 ==//
 
 
 function sortChange(e) {
-  showData.sort(function (a, b) {
+  filterData.sort(function (a, b) {
     return a[e] - b[e];
   });
-  renderData(showData);
-} //進階篩選
+  update(filterData);
+} //== 進階篩選  表單內排序 ==//
 
-
-tableSortGroup.addEventListener('click', sortTableBytitle, false);
 
 function sortTableBytitle(e) {
   if (e.target.nodeName !== "I") {
@@ -172,12 +165,12 @@ function sortTableBytitle(e) {
     var sortDirection = e.target.dataset.sort;
 
     if (sortDirection === "up") {
-      showData.sort(function (a, b) {
-        return a[sortTitle] - b[sortTitle]; //內容物需要研究
+      filterData.sort(function (a, b) {
+        return b[sortTitle] - a[sortTitle];
       });
     } else if (sortDirection === "down") {
-      showData.sort(function (a, b) {
-        return b[sortTitle] - a[sortTitle];
+      filterData.sort(function (a, b) {
+        return a[sortTitle] - b[sortTitle];
       });
     }
 
@@ -185,11 +178,11 @@ function sortTableBytitle(e) {
     selectList.forEach(function (item, index) {
       selectList[index].value = "\u4F9D".concat(sortTitle, "\u6392\u5E8F");
     });
-    renderData(showData);
+    update(filterData);
   }
 
   ;
-} //清空selectList
+} //== 清空 下拉選單 ==//
 
 
 function resetSelect() {
